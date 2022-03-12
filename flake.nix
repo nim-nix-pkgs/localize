c8253fe900633041ea7e553ff254b02f1a663f70
hugosenari <hugosenari@gmail.com>
2022-03-12 03:04:25 -0300

    chore: re index localize

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = ''Compile time localization for applications'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."localize-master".url = "path:./master";
+  inputs."localize-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."localize-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."localize-0_1".url = "path:./0_1";
+  inputs."localize-0_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."localize-0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."localize-0_2".url = "path:./0_2";
+  inputs."localize-0_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."localize-0_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Compile time localization for applications'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-localize-master.flake = false;
+  inputs.src-localize-master.owner = "levovix0";
+  inputs.src-localize-master.ref   = "refs/heads/master";
+  inputs.src-localize-master.repo  = "localize";
+  inputs.src-localize-master.type  = "github";
+  
+  inputs."fusion".dir   = "nimpkgs/f/fusion";
+  inputs."fusion".owner = "riinr";
+  inputs."fusion".ref   = "flake-pinning";
+  inputs."fusion".repo  = "flake-nimble";
+  inputs."fusion".type  = "github";
+  inputs."fusion".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fusion".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-localize-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-localize-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
